53df7400e03858e99f18cec994699f979e3d9cef
wathanaplam <plamzxsoe@gmail.com>
2025-02-06 23:51:53 +0700

    Add code for excercise 4 python

--- Exercise4_Wathanakun_S.py
+++ Exercise4_Wathanakun_S.py
@@ -1,1 +1,14 @@
 
+foundationEngScore = float(35.5)
+generalBusinessScore = float(25.75)
+computerSystemScore = float(45.7)
+computerProgrammingScore = float(47)
+
+print("--- Your Score ---")
+print("Foundation English :", foundationEngScore)
+print("General Business :", generalBusinessScore)
+print("Introduction to Computer Systems :", computerSystemScore)
+print("Computer Programming :", computerProgrammingScore)
+
+
+
