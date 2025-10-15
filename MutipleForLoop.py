52f1acfb8156b11a9e7b928f339e2177307f32c3
Wathanakun Sae-kee <plamzxsoe@gmail.com>
2025-10-15 13:52:01 +0700

    Add main function

--- MutipleForLoop.py
+++ MutipleForLoop.py
@@ -1,26 +1,23 @@
 
-number = int(input("Enter the number: "))
+def main ():
+    number = int(input("Enter the number: "))
+    x = 0
 
-x = 0
-
-if number == 0:
-    for number in range(12):
-        if number == 0:
-            number = 2
-        else:
-            number = number + 1
+    if number == 0:
+        for number in range(12):
+            if number == 0:
+                number = 2
+            else:
+                number = number + 1
+            for x in range(12):
+                x = x + 1
+                y = number * x
+                print(number, " X ", x, " = ", y)
+    else:
         for x in range(12):
             x = x + 1
             y = number * x
             print(number, " X ", x, " = ", y)
-else:
-    for x in range(12):
-        x = x + 1
-        y = number * x
-        print(number, " X ", x, " = ", y)
-    
 
-#for x in range(12):
-#    x = x + 1
-#    y = number * x
-#    print(number, " X ", x, " = ", y)
+if __name__ == "__main__":
+    main()
